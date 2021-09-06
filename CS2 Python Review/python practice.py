41c8be194df749ead0c2d4ec68694d85370a1dad
Jack Komaroff <jackkomaroff23@gmail.com>
2021-09-06 17:26:46 -0400

    practice

---
+++ CS2 Python Review/python practice.py
@@ -0,0 +1,18 @@
+print(2 ** 5)
+print(2 * 5)
+int1 = 2 ** 5
+print(int1)
+print("int1")
+print("Hello, world")
+first = "Hello"
+second = "world"
+print(first + " " + second)
+print("a" * 5)
+nums = list(range(0,100000))
+
+b = int(input("Number base\n"))
+y = int(input("Number exponent\n"))
+
+def raisePower():
+    return b**y
+print(raisePower())
