7c73fcd174b10843935cf174da89cdfbcae4c3c4
Jack Komaroff <jackkomaroff23@gmail.com>
2022-03-31 11:39:42 -0400

    pi

---
+++ Python/pi.py
@@ -0,0 +1,16 @@
+import random
+import math
+
+numerator=0
+denominator=0
+for i in range(1,1000000):
+    x = random.uniform(0,1)
+    y = random.uniform(0,1)
+    if math.sqrt(x**2+y**2)<=1:
+        numerator+=1
+        denominator+=1
+    else:
+        denominator+=1
+
+pi_estimate=4*(numerator/denominator)
+print(pi_estimate)
